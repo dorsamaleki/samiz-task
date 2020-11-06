dc22a1986c3fffc4f4836389c069c48c1ce48775
Dorsa Maleki <a.malekidorsa@gmail.com>
2020-11-06 12:13:22 +0330

    Updates

---
+++ precache-manifest.88cbdca829b9b0a8297bb11e6e56f82c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "829963e9cfbdaae16305bf0674af5a54",
+    "url": "/dorsamaleki/samiz-task.git/index.html"
+  },
+  {
+    "revision": "6fe3d64b8dc4aacf2e44",
+    "url": "/dorsamaleki/samiz-task.git/static/css/main.6eea14c9.chunk.css"
+  },
+  {
+    "revision": "e0eea8f3be26920b2872",
+    "url": "/dorsamaleki/samiz-task.git/static/js/2.f6608941.chunk.js"
+  },
+  {
+    "revision": "973693e77d1f882650481a8433ca9257",
+    "url": "/dorsamaleki/samiz-task.git/static/js/2.f6608941.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fe3d64b8dc4aacf2e44",
+    "url": "/dorsamaleki/samiz-task.git/static/js/main.887cee9a.chunk.js"
+  },
+  {
+    "revision": "5178187c22364d32e290",
+    "url": "/dorsamaleki/samiz-task.git/static/js/runtime-main.e0e5e0e1.js"
+  }
+]);
